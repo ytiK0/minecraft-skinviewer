17749f5dc14b9e4c3058a714c426e8a4b9873844
ytiK0 <eeeee@eee.eee>
2025-05-30 15:10:33 +0300

    player viewer update

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,27 +1,68 @@
-import {useTexture} from "@react-three/drei";
-import {MeshStandardMaterial, NearestFilter, SRGBColorSpace} from "three";
-import {useMemo} from "react";
+import {CanvasTexture, MeshStandardMaterial, NearestFilter, SRGBColorSpace} from "three";
+import {useEffect, useMemo, useState} from "react";
 import {Arms, Body, Head, Legs} from "./SkinPartComponents";
+import {EarsContext} from "../context/EarsContext.tsx";
+import {decodeEarsSkin, defaultEarsContextValue} from "../utils/decodeEarsSkin.ts";
+import type {EarsContextValue} from "../types";
+import {SkinMaterialContext} from "../context/SkinContext.tsx";
+import {defaultLayersConfig, LayersContext} from "../context/LayersContext.tsx";
 
-export default function Player() {
+export default function Player({pathToSkin}: {pathToSkin: string}) {
   // TODO: add slim support
+  const [ears, setEars] = useState<EarsContextValue>(defaultEarsContextValue);
 
-  const skin = useTexture("/skin.png", (skin) => {
-    skin.magFilter = NearestFilter;
-    skin.colorSpace = SRGBColorSpace
-  });
+  const skinCanvas = useMemo(() => {
+    const canvas = document.createElement("canvas")
+    canvas.height = 64;
+    canvas.width = 64;
+
+    return canvas;
+  }, [])
+
+  const skin = useMemo(() => {
+    const skinTexture = new CanvasTexture(skinCanvas);
+    skinTexture.magFilter = NearestFilter;
+    skinTexture.minFilter = NearestFilter;
+    skinTexture.colorSpace = SRGBColorSpace;
+
+    return skinTexture;
+  }, [skinCanvas]);
 
   const skinMaterial = useMemo(() => new MeshStandardMaterial({
     map: skin,
     transparent: true,
   }), [skin]);
 
+
+  useEffect(() => {
+    const skinImg = new Image();
+    const ctx = skinCanvas.getContext("2d")!;
+
+    skinImg.onload = () => {
+      skinCanvas.width = skinImg.width;
+      skinCanvas.height = skinImg.height;
+      ctx.drawImage(skinImg, 0, 0);
+      skin.needsUpdate = true;
+
+      const earsData = new DataView(ctx.getImageData(0, 32, 4, 4).data.buffer);
+      setEars(decodeEarsSkin(earsData));
+    }
+
+    skinImg.src = pathToSkin;
+  }, [skin, pathToSkin]);
+
   return (
     <>
-      <Head skinMaterial={skinMaterial} />
-      <Body skinMaterial={skinMaterial} />
-      <Arms skinMaterial={skinMaterial}  />
-      <Legs skinMaterial={skinMaterial} />
+      <LayersContext.Provider value={defaultLayersConfig}>
+        <SkinMaterialContext.Provider value={skinMaterial}>
+          <EarsContext.Provider value={ears}>
+            <Head />
+            <Body />
+            <Arms />
+            <Legs />
+          </EarsContext.Provider>
+        </SkinMaterialContext.Provider>
+      </LayersContext.Provider>
     </>
   );
 }
