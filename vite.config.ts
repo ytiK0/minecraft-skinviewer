7caaa92b7987d615f8c73b314f85f6692d6ce182
ytiK0 <eeeee@eee.eee>
2025-05-31 14:49:24 +0300

    bundle visualisation enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
   css: {
     modules: {
       localsConvention: "camelCase",
       generateScopedName: "[name]_[local]__[hash:base64:3]"
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('three')) return 'three';
+          if (id.includes('react-reconciler')) return 'react-core';
+          if (id.includes('node_modules')) return 'vendor';
+        },
+      },
+    },
   }
 })
