41cb3b0d76b26ff5710aeb509783320c311f368a
ytiK0 <eeeee@eee.eee>
2025-06-01 15:03:43 +0300

    DebugSphere feature added

--- src/components/SkinPartComponents/Arm.tsx
+++ src/components/SkinPartComponents/Arm.tsx
@@ -2,31 +2,36 @@ import type {LimbMap, LimbSkinPartProps} from "../../types";
 import {lArm, lArmLayer, rArm, rArmLayer} from "../../geometry/skinPartGeometry/arms.ts";
 import {useSkinMaterial} from "../../context/SkinContext.tsx";
 import {ClawRenderer} from "../EarsComponents/ClawRenderer.tsx";
+import DebugSphere from "../DebugSphere.tsx";
 
 const limbMap: LimbMap = {
   "left": {
     base: lArm,
     layer: lArmLayer,
-    defaultPosition: [6, 0, 0] as const
+    defaultPosition: [6, 4.5, 0] as const
   },
   "right":  {
     base: rArm,
     layer: rArmLayer,
-    defaultPosition: [-6, 0, 0] as const
+    defaultPosition: [-6, 4.5, 0] as const
   }
 }
 
-export function Arm({ position, hideLayer, side }: LimbSkinPartProps) {
+export function Arm({ position, hideLayer, side, debug }: LimbSkinPartProps) {
   const skinMaterial = useSkinMaterial();
 
   return (
-    <group name={`${side}Arm`} position={position || limbMap[side].defaultPosition}>
-      <mesh geometry={limbMap[side].base} material={skinMaterial} renderOrder={0}/>
-      <mesh geometry={limbMap[side].layer} material={skinMaterial} visible={!hideLayer} renderOrder={2}/>
-
-      <group name={"ears"}>
-        <ClawRenderer clawSide={side === "left" ? "lArm" : "rArm"} />
+    <object3D name={`${side}Arm`} position={position || limbMap[side].defaultPosition}>
+      { debug && <DebugSphere/> }
+      <group position={[0,-4.5, 0]} >
+        <mesh
+          geometry={limbMap[side].base} material={skinMaterial} renderOrder={0}/>
+        <mesh
+          geometry={limbMap[side].layer} material={skinMaterial} visible={!hideLayer} renderOrder={2}/>
+        <group name={"ears"}>
+          <ClawRenderer clawSide={side === "left" ? "lArm" : "rArm"} />
+        </group>
       </group>
-    </group>
+    </object3D>
   );
 }
--- src/components/SkinPartComponents/Leg.tsx
+++ src/components/SkinPartComponents/Leg.tsx
@@ -2,30 +2,34 @@ import type {LimbMap, LimbSkinPartProps} from "../../types";
 import {lLeg, lLegLayer, rLeg, rLegLayer} from "../../geometry/skinPartGeometry/legs.ts";
 import {useSkinMaterial} from "../../context/SkinContext.tsx";
 import {ClawRenderer} from "../EarsComponents/ClawRenderer.tsx";
+import DebugSphere from "../DebugSphere.tsx";
 
 const limbMap: LimbMap = {
   "left": {
     base: lLeg,
     layer: lLegLayer,
-    defaultPosition: [-2, 0, 0] as const
+    defaultPosition: [-2, 4.5, 0] as const
   },
   "right":  {
     base: rLeg,
     layer: rLegLayer,
-    defaultPosition: [2, 0, 0] as const
+    defaultPosition: [2, 4.5, 0] as const
   }
 }
 
-export function Leg({ position, hideLayer, side }: LimbSkinPartProps) {
+export function Leg({ position, hideLayer, side, debug }: LimbSkinPartProps) {
   const skinMaterial = useSkinMaterial();
 
   return (
     <group name={`${side}Leg`} position={position || limbMap[side].defaultPosition}>
-      <mesh geometry={limbMap[side].base} material={skinMaterial} renderOrder={0}/>
-      <mesh geometry={limbMap[side].layer} material={skinMaterial} visible={!hideLayer} renderOrder={1}/>
+      { debug && <DebugSphere/> }
+      <group position={[0,-4.5,0]}>
+        <mesh geometry={limbMap[side].base} material={skinMaterial} renderOrder={0}/>
+        <mesh geometry={limbMap[side].layer} material={skinMaterial} visible={!hideLayer} renderOrder={1}/>
 
-      <group name={"ears"}>
-        <ClawRenderer clawSide={side === "left" ? "lLeg" : "rLeg"}/>
+        <group name={"ears"}>
+          <ClawRenderer clawSide={side === "left" ? "lLeg" : "rLeg"}/>
+        </group>
       </group>
     </group>
   );
