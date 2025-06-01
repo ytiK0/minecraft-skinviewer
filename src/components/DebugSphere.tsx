ae7c1b5f4ef08b64258aa849ad7df14ca743053c
ytiK0 <eeeee@eee.eee>
2025-06-01 15:04:24 +0300

    feature: now renders on top of any mesh (xray)

--- src/components/DebugSphere.tsx
+++ src/components/DebugSphere.tsx
@@ -1,4 +1,5 @@
 import {Sphere} from "@react-three/drei";
+import {MeshBasicMaterial} from "three";
 
 interface DebugSphereProps {
   sphereSize?: number,
@@ -8,11 +9,20 @@ interface DebugSphereProps {
 const DEFAULT_SPHERE_SIZE = 0.5;
 const DEFAULT_AXES_SIZE = 5;
 
+const sphereMaterial = new MeshBasicMaterial({
+  color: "#2aeeff",
+  wireframe: true,
+  depthTest: false,
+  depthWrite: false,
+  transparent: true,
+  opacity: 1.0
+});
+
 export default function DebugSphere({sphereSize, axesSize}: DebugSphereProps) {
   return (
     <group name={"debug-sphere"}>
       <axesHelper args={[axesSize || DEFAULT_AXES_SIZE]} />
-      <Sphere args={[sphereSize || DEFAULT_SPHERE_SIZE]} />
+      <Sphere args={[sphereSize || DEFAULT_SPHERE_SIZE]} renderOrder={999} material={sphereMaterial} />
     </group>
   );
 }
