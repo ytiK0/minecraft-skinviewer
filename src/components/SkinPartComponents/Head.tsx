169c46b98965f4c33b1d8997f2875a21d68f6754
ytiK0 <eeeee@eee.eee>
2025-06-01 15:06:29 +0300

    snout now render

--- src/components/SkinPartComponents/Head.tsx
+++ src/components/SkinPartComponents/Head.tsx
@@ -5,6 +5,7 @@ import {useSkinMaterial} from "../../context/SkinContext.tsx";
 import {useLayers} from "../../context/LayersContext.tsx";
 import {EarRenderer} from "../EarsComponents/ear/EarRenderer.tsx";
 import {HornRenderer} from "../EarsComponents/HornRenderer.tsx";
+import {SnoutRender} from "../EarsComponents/snout/SnoutRender.tsx";
 const defaultPosition = new Vector3(0,28 ,0);
 
 export function Head({ position }: SkinPartProps) {
@@ -22,6 +23,7 @@ export function Head({ position }: SkinPartProps) {
 
       <group name={"ears"} >
         <EarRenderer />
+        <SnoutRender />
         <HornRenderer />
       </group>
     </group>
