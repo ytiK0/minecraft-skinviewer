62ead047dbadb69c189e09a987a04e96b7cda1bd
ytiK0 <eeeee@eee.eee>
2025-06-01 19:20:14 +0300

    axis moved into player component, and it shows in debug mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ function App() {
   return (
     <>
       <Canvas style={{height: "100vh"}} camera={{position: [0, 16, 40], near: 1, far: 80}}>
-        <axesHelper args={[100]}/>
         <ambientLight intensity={1.5}/>
-        <OrbitControls target={[0, 16, 0]} enablePan={false}/>
-        <Player pathToSkin={'/ears.png'}/>
+        <OrbitControls target={[0, 16, 0]} enablePan={false} minDistance={10} maxDistance={50}/>
+        <Player pathToSkin={'/skin.png'}/>
       </Canvas>
     </>
   )
