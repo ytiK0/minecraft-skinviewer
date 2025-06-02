309a8c95243e377dc623bb31db3b2f97cf0ead28
ytiK0 <eeeee@eee.eee>
2025-06-02 18:24:07 +0300

    lib exports

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// SkinViewer
+export { SkinViewer } from './components/SkinPreview/SkinViewer';
+
+// SkinPartComponents
+export * from './components/SkinPartComponents';
+
+// EarsComponents
+export * from './components/EarsComponents/ClawRenderer';
+export * from './components/EarsComponents/HornRenderer';
+export * from './components/EarsComponents/ear/EarRenderer';
+export * from './components/EarsComponents/snout/SnoutRender';
+export * from './components/EarsComponents/tail/TailRenderer';
+export * from './components/EarsComponents/tail/SegmentedTail';
+export * from './components/EarsComponents/tail/TailRoot';
+
+// Contexts
+export * from './context/LayersContext';
+export * from './context/EarsContext';
+export * from './context/SkinContext'; 
