0fc9f22c4787534cdce994298d996df2312e485c
ytiK0 <eeeee@eee.eee>
2025-06-02 12:19:55 +0300

    simple skin change demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import {SkinViewer} from "./components/SkinPreview/SkinViewer.tsx";
+import {type ChangeEventHandler, useCallback, useState} from "react";
 
 function App() {
+  const [pathToSkin, setPathToSkin] = useState("/skin.png");
+
+  const handleChange: ChangeEventHandler = useCallback((ev) => {
+    const target = ev.target as HTMLInputElement;
+    setPathToSkin(target.value);
+  }, [])
+
   return (
     <>
-      <SkinViewer />
+      <SkinViewer pathToSkin={pathToSkin} />
+      <input onChange={handleChange} value={pathToSkin}/>
     </>
   )
 }
