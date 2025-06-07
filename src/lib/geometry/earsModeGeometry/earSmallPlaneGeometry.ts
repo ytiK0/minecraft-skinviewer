a1c898372145e8e5c4eceb3826933a6c527ea949
ytiK0 <eeeee@eee.eee>
2025-06-07 17:50:53 +0300

    fix uv coordinates

--- src/lib/geometry/earsModeGeometry/earSmallPlaneGeometry.ts
+++ src/lib/geometry/earsModeGeometry/earSmallPlaneGeometry.ts
@@ -2,12 +2,12 @@ import {EarsComponentGeometry} from "../EarsComponentGeometry.ts";
 
 const leftEarSmallPlaneGeometry = new EarsComponentGeometry(8, 4, {
   front: [36, 16, 8, 4],
-  back: [12, 16, 8, 4]
+  back: [12, 16, 8, 4, "none", "y"]
 });
 
 const rightEarSmallPlaneGeometry = new EarsComponentGeometry(8, 4, {
   front: [36, 32, 8, 4],
-  back: [12, 32, 8, 4]
+  back: [12, 32, 8, 4, "none", "y"]
 });
 
 export {leftEarSmallPlaneGeometry, rightEarSmallPlaneGeometry};
