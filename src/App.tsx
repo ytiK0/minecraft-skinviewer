f45a5628c0fc2a7472e0b647c7ddb7f2ff4d8e40
ytiK0 <eeeee@eee.eee>
2025-06-07 17:52:58 +0300

    demo app has now more control

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,79 @@
-import {SkinViewer} from "./lib/components/SkinPreview/SkinViewer.tsx";
-import {type ChangeEventHandler, useCallback, useState} from "react";
+import {SkinViewer} from "./lib/components/SkinViewer/SkinViewer.tsx";
+import {type ChangeEventHandler, type FormEventHandler, Fragment, useCallback, useState} from "react";
+import {getLayersDefaultConfig, type LayersContextValue} from "./lib/context/LayersContext";
 
 function App() {
-  const [pathToSkin, setPathToSkin] = useState("/skin.png");
+  const [pathToSkin, setPathToSkin] = useState("https://mc-heads.net/skin/c0rqi");
+  const [isSlim, setIsSlim] = useState(true);
+  const [layers, setLayers] = useState(getLayersDefaultConfig());
 
   const handleChange: ChangeEventHandler = useCallback((ev) => {
     const target = ev.target as HTMLInputElement;
     setPathToSkin(target.value);
-  }, [])
+  }, []);
+
+  const handleIsSlimChange: ChangeEventHandler = useCallback((ev) => {
+    const target = ev.target as HTMLInputElement;
+    setIsSlim(target.checked);
+  }, []);
+
+  const handleLayerChange: FormEventHandler<HTMLFormElement> = useCallback((event) => {
+    const target = event.target as HTMLInputElement;
+
+    if (target.type !== "checkbox") {
+      return;
+    }
+
+    const partName = target.dataset["part"] as keyof LayersContextValue;
+
+    if (!partName) {
+      console.warn("Checkbox dose not have part name data attribute");
+      return;
+    }
+
+    if (!(partName in layers)) {
+      console.warn("Checkbox does not have valid part name", partName);
+      return;
+    }
+
+    const layerName = target.dataset["layer"];
+
+    if (layerName === "base") {
+      setLayers((prev) => ({...prev, [partName]: { ...prev[partName], isBaseVisible: target.checked }}))
+    }
+    else if (layerName === "overlay") {
+      setLayers((prev) => ({...prev, [partName]: { ...prev[partName], isOverlayVisible: target.checked }}))
+    }
+    else {
+      console.warn("Checkbox does not provide layer data attribute")
+    }
+  }, []);
 
   return (
     <>
-      <SkinViewer skinSrc={pathToSkin} />
+      <SkinViewer skinSrc={pathToSkin} isSlim={isSlim} layers={layers} />
       <input onChange={handleChange} value={pathToSkin}/>
+      <label htmlFor={"is-slim"}>
+        <input id={"is-slim"} onChange={handleIsSlimChange} type={"checkbox"} defaultChecked />
+        is slim
+      </label>
+      <form onChange={handleLayerChange} style={{display: "grid", gridTemplateColumns: "repeat(3, 80px)"}}>
+        {
+          (Object.keys(layers) as (keyof LayersContextValue)[]).map((skinPartName) => (
+            <Fragment key={skinPartName}>
+              <span>{skinPartName[0].toLowerCase() + skinPartName.slice(1)}</span>
+              <label htmlFor={`${skinPartName}-base`}>
+                <input id={`${skinPartName}-base`} type={"checkbox"} defaultChecked data-part={skinPartName} data-layer={"base"}/>
+                Base
+              </label>
+              <label htmlFor={`${skinPartName}-overlay`}>
+                <input id={`${skinPartName}-overlay`} type={"checkbox"} defaultChecked data-part={skinPartName} data-layer={"overlay"}/>
+                Overlay
+              </label>
+            </Fragment>
+          ))
+        }
+      </form>
     </>
   )
 }
