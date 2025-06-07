aacb37254e57e349e8e9c8709a8a5e0ff366cdcc
ytiK0 <eeeee@eee.eee>
2025-06-07 17:50:37 +0300

    slim support added

--- src/lib/components/Player.tsx
+++ src/lib/components/Player.tsx
@@ -1,11 +1,11 @@
 import {Arms, Body, Head, Legs} from "./SkinPartComponents";
 
-export default function Player({ debug }: { debug?: boolean }) {
+export default function Player({ debug, isSlim }: { isSlim?: boolean, debug?: boolean }) {
   return (
     <group name={"playerModel"}>
       <Head debug={debug}/>
       <Body debug={debug}/>
-      <Arms debug={debug}/>
+      <Arms debug={debug} isSlim={isSlim} />
       <Legs debug={debug}/>
       { debug && <axesHelper args={[100]}/> }
     </group>
